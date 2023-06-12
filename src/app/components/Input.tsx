cb412dca46b7f41a345626eccde797569f4795fa
Priscila Angel Rodrigues Silva <priangel@pop-os.localdomain>
2023-06-12 19:32:04 -0300

    Finaliza componente de input

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,19 +1,34 @@
 import TextField from "@mui/material/TextField";
 
 type TextFieldProps = {
-    required: boolean;
+    required ?: boolean;
     label: string;
     valueChange: Function;
+    disabled ?: boolean;
+    type: string;
+    placeholder: string;
+    error: boolean;
+    helperText ?: string;
+    errorMessage ?: string;
 }
 
 export function Input(props: TextFieldProps) {
     return (
-        <TextField 
-            id="outlined-basic" 
-            error={props.required} 
-            onChange = {e => props.valueChange(e.target.value)} 
-            label={props.label} variant="outlined" 
-            required={props.required} 
-        />
+        <div className="flex flex-col">
+            <label htmlFor="outlined-basic" className="font-bold">
+                {props.label} {props.required ? '*' : ''}
+            </label>
+            <TextField
+                id="outlined-basic" 
+                error={props.error} 
+                onChange = {e => props.valueChange(e.target.value)} 
+                label="" variant="outlined" 
+                required={props.required} 
+                disabled={props.disabled}
+                type={props.type}
+                helperText={props.error ? props.errorMessage : props.helperText} 
+                placeholder={props.placeholder}
+            />
+        </div>
     )
 }
