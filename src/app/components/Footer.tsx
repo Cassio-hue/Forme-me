316e797e433bae6143850e76c4f02d6bd727eabe
Eduardo Ferreira <eduardo.ferreira.mc.edu@gmail.com>
2023-06-13 02:41:23 -0300

    initial display

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,10 @@
-import { FaHeart } from "react-icons/fa"
+import { FaHeart } from 'react-icons/fa'
 
 export function Footer() {
     return (
-        <footer
-            className='flex justify-center px-8 bg-verde font-alegreya text-white text-md font-bold h-16 items-center fixed bottom-0 w-full'
-        >
-            <span>
-                DESENVOLVIDO COM
-            </span>
-            <FaHeart className='mx-2' color='red'/>
+        <footer className="flex justify-center px-8 bg-verde font-alegreya text-white text-md font-bold h-16 items-center fixed bottom-0 w-full">
+            <span>DESENVOLVIDO COM</span>
+            <FaHeart className="mx-2" color="red" />
             <span>PELOS ASTRONAUTAS!</span>
         </footer>
     )
