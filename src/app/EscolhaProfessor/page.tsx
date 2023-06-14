e2feac07b8065236dcd492f42a98cf2448cc000d
Eduardo Ferreira <eduardo.ferreira.mc.edu@gmail.com>
2023-06-14 20:02:15 -0300

    feat: cards

--- src/app/EscolhaProfessor/page.tsx
+++ src/app/EscolhaProfessor/page.tsx
@@ -1,15 +1,25 @@
 'use client'
 
 import { CardProfessor } from '../components/CardProfessor'
+import { CardTrabalho } from '../components/CardTrabalho'
 
 export default function Page() {
     return (
-        <CardProfessor
-            name={'Maycon'}
-            disciplines={['Droga um', 'droga dois']}
-            area_of_interest={['seta doido', 'seta dois']}
-            area_of_ocupation={['beterraba um', 'brocolis dois']}
-            image={'https://loremflickr.com/640/480'}
-        />
+        <div className="flex-grow overflow-y-auto">
+            <CardTrabalho
+                names={['Maycon']}
+                orientadores={['Droga um', 'droga dois']}
+                coorientadores={['seta doido', 'seta dois']}
+                related_fields={['beterraba um', 'brocolis dois']}
+                title={'Como namorar o Maycon'}
+            />
+            <CardProfessor
+                name={'Maycon'}
+                disciplines={['Droga um', 'droga dois']}
+                area_of_interest={['seta doido', 'seta dois']}
+                area_of_ocupation={['beterraba um', 'brocolis dois']}
+                image={'https://loremflickr.com/640/480'}
+            />
+        </div>
     )
 }
