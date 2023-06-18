fb3e25a9bab136c5775d982ff77aef816d87f6cd
Jose0908 <joserisoares2001@gmail.com>
2023-06-18 20:37:48 -0300

    feat: Pagina de suporte, botao atualizado e buscar professor definido como pagina principal

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -5,7 +5,7 @@ export const FormButton = ({ children }: any) => {
     return (
         <Button
             className={clsx(
-                'bg-azul hover:bg-azul hover:backdrop: text-white rounded-lg normal-case p-3 shadow-lg shadow-slate-600 mt-3'
+                'bg-azul hover:bg-azul hover:backdrop: text-white rounded-lg normal-case p-3 shadow-lg shadow-slate-600 mt-3 w-full'
             )}
         >
             {children}
