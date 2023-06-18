16be22cf12cdebdbd38e8942137bfd47e46b1752
Cassio-hue <cassiocvtb2003@gmail.com>
2023-06-18 16:49:14 -0300

    fix: lint problems

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,8 +1,14 @@
 import { Button } from '@mui/material'
 import clsx from 'clsx'
 
-export const FormButton = ( {children}:any ) => {
+export const FormButton = ({ children }: any) => {
     return (
-        <Button className={clsx("bg-azul hover:bg-azul hover:backdrop: text-white rounded-lg normal-case p-3 shadow-lg shadow-slate-600 mt-3")}>{children}</Button>
+        <Button
+            className={clsx(
+                'bg-azul hover:bg-azul hover:backdrop: text-white rounded-lg normal-case p-3 shadow-lg shadow-slate-600 mt-3'
+            )}
+        >
+            {children}
+        </Button>
     )
 }
